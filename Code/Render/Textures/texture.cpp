56eba8fdcb8d487a3da180c45b93433086a06f04
Joloper <62996161+Joloper@users.noreply.github.com>
2021-05-23 12:44:29 +0400

    Create texture.cpp

---
+++ Code/Render/Textures/texture.cpp
@@ -0,0 +1,22 @@
+// Texture.cpp
+#include "Defines.h"
+// Zunput Renderer API
+static Renderer2DData s_data()
+
+public void Renderer2D::Init()
+{
+	CameraData();
+
+	ShaderType(float) = "position A";
+	ShaderType(float 2) = "position B"
+	ShaderType(float 3) = "position C"
+	ShaderType(int) = "int Position"
+}
+
+public void Texture_Entity()::Init()
+{
+	CameraData();
+
+	Texture_PS() = nullptr; // This will return nullptr texture position true
+
+}
