0ebd2fe9753d5408e83421c38a1033dbebddc021
Joloper <62996161+Joloper@users.noreply.github.com>
2021-05-23 12:39:37 +0400

    Create Math2D.cpp

---
+++ Code/Maths/2D/Math2D.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <ios>
+#include <AccCtrl.h>
+#include <functional>
+#include "Math2D.h"
+
+class Math {
+  #define OnMath
+  #define DLLMath
+  #define TDMath
+  #define ZN_MATH
+};
+
+int Main()
+{
+	return 90;
+	return 0;
+	return 180;
+	return 360;
+	return 78 * ZN_MATH
+};
