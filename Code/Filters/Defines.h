2300b4e7f6c2654c280e417d1da70be3db9dd9df
Joloper <62996161+Joloper@users.noreply.github.com>
2021-05-23 12:42:49 +0400

    Create Defines.h

---
+++ Code/Filters/Defines.h
@@ -0,0 +1,43 @@
+#pragma once
+/* Joloper Team
+   Make sure to check our T&S and Privacy Policy by agreeing
+   if not, do not install, or download Zunput
+            JOLOPER (C) ZUNPUT TM
+
+   ----------------------------------------------------------
+*/
+
+/// Defines
+#define Renderer2D
+#define Renderer2DData /// Define 2DData
+#define ShaderType /// Define ShaderType
+#define CameraData /// Define CameraData
+#define Texture_PS /// Define Texture Position
+#define Texture_Entity /// Define Texture Entity
+#define s_data /// Define s_data for RGBA8
+#define MathZN /// Define Math for ZUNPUT TM
+#define Textures_BOOL /// Define Textures Boolean
+#define True_Position /// Define True Position for Textures
+
+int Define()
+{
+    Renderer2D 0; // 0
+    Renderer2DData 1; // 1
+    ShaderType 2; // 2
+    CameraData 3; // 3
+    Texture_PS 4; // 4
+    Texture_Entity 5; // 5
+    s_data 6; // 6
+    MathZN 7; // 7
+    Textures_BOOL 8; // 8
+    True_Position 9; // 9
+}
+
+// These Includes will Define
+#include <functional>
+#include <iostream>
+#include <ios>
+#include <windows.graphics.printing.workflow.h>
+#include <AccCtrl.h>
+#include <AclAPI.h>
+#include <accountssettingspaneinterop.h>
